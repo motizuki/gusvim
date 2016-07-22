c050ef20d4da601165749a31c269315b1ed10a6f
Gustavo Motizuki <gustavokm90@gmail.com>
2016-07-23 00:25:39 +1200

    remove plugin auto install

--- install.sh
+++ install.sh
@@ -7,10 +7,3 @@ echo "\033[1;32m Trying to create symbolic links to .vim \033[0m"
 ln -s ~/gusvim ~/.vim
 echo "\033[1;32m Installing Dein... \033[0m"
 sh dein-install.sh ~/.vim/bundle
-
-echo ''
-echo ''
-echo "\033[1;31m IGNORE ERRORS BELOW - IT SHOULD GO AWAY WHEN DEIN INSTALL ALL PLUGINS \033[0m"
-echo ''
-echo ''
-vim -c 'call dein#update()'
