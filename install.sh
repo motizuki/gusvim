2e4676bdb883e913e0ee99266e8a7c97eeaa030d
Gustavo Motizuki <gustavokm90@gmail.com>
2016-07-22 23:47:53 +1200

    update install script

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
+cat ~/gusvim/banner.txt
+echo 'Doing something...'
+
 ln -s ~/gusvim/vimrc ~/.vimrc
 ln -s ~/gusvim ~/.vim
 sh dein-install.sh ~/.vim/bundle
+vim -u ~/gusvim/dein-only-vimrc -c 'call dein#update()'
