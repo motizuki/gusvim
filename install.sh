2719e711590fc958d86b6b3c6171c4500b750801
Gustavo Motizuki <gustavokm90@gmail.com>
2016-07-23 00:09:49 +1200

    Adding colors

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 cat ~/gusvim/banner.txt
-echo 'Doing something...'
+echo "\033[1;34m Installing... \033[0m"
 
+echo "\033[1;32m Trying to create symbolic links to .vimrc \033[0m"
 ln -s ~/gusvim/vimrc ~/.vimrc
+echo "\033[1;32m Trying to create symbolic links to .vim \033[0m"
 ln -s ~/gusvim ~/.vim
+echo "\033[1;32m Installing Dein... \033[0m"
 sh dein-install.sh ~/.vim/bundle
 
 echo ''
