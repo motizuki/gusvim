400cc0c73ea10131469fb9f2dc262cece7ea4523
Gustavo Motizuki <gustavokm90@gmail.com>
2016-07-22 23:51:08 +1200

    remove dein-only

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ echo 'Doing something...'
 ln -s ~/gusvim/vimrc ~/.vimrc
 ln -s ~/gusvim ~/.vim
 sh dein-install.sh ~/.vim/bundle
-vim -u ~/gusvim/dein-only-vimrc -c 'call dein#update()'
+vim -c 'call dein#update()'
