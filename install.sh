898d3deee0dcbf699648b5fec6cba7a9750af8e4
Gustavo Motizuki <gustavokm90@gmail.com>
2016-07-23 00:01:11 +1200

    Adding instructions

--- install.sh
+++ install.sh
@@ -4,4 +4,10 @@ echo 'Doing something...'
 ln -s ~/gusvim/vimrc ~/.vimrc
 ln -s ~/gusvim ~/.vim
 sh dein-install.sh ~/.vim/bundle
+
+echo ''
+echo ''
+echo "\033[1;31m IGNORE ERRORS BELOW - IT SHOULD GO AWAY WHEN DEIN INSTALL ALL PLUGINS \033[0m"
+echo ''
+echo ''
 vim -c 'call dein#update()'
